9d2a918ff521eabd957fb518ad61370337116e15
Caroline STone <docphilstone@gmail.com>
2020-06-25 20:04:44 -0500

    commit

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,7 +2,10 @@ package org.launchcode.codingevents.models;
 
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -15,6 +18,8 @@ import java.util.Objects;
 @Entity
 public class Event {
 
+    @Id
+    @GeneratedValue
     private int id;
     private static int nextId = 1;
 
